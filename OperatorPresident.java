8d8445e0e2481d7539934a51cf0e31ae7afbaa73
gauravkamat17 <kamatgaurav17@gmail.com>
2024-07-21 07:06:23 +0000

    Learning java

---
+++ OperatorPresident.java
@@ -0,0 +1,30 @@
+class OperatorPresident{
+    public static void main(String args[]){
+        // President And Associtivativity;
+        // int a =58*6-7/4;
+        // int b =58/6-7*7;
+        // System.out.println(a);
+        // System.out.println(b);
+        // int x = 45;
+        // int y = 9;
+   
+        // int a= 6;
+        // int b=2;
+        // int c=9;
+        // int h=(x-y)/2;
+        // int g=b*b-4*a*c/2*a;
+        // System.out.println(h);
+        // System.out.println(g);
+        int u = 78;
+        int v = 88;
+        int x = u*u-v*v;
+        int a = 75;
+        int b = 45;
+        int d = 12;
+        int m = a*b-d;
+        System.out.println(x);
+        System.out.println(m); 
+
+
+    }
+}
