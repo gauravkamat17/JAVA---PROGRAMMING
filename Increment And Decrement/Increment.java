13aee325ef12e45e54be043c9490c29203474b7c
gauravkamat17 <kamatgaurav17@gmail.com>
2024-07-21 16:00:36 +0000

    Java Programming

---
+++ Increment And Decrement/Increment.java
@@ -0,0 +1,18 @@
+class Increment{
+    public static void main(String args[]){
+        /*byte x = 6;
+        int y = 6;
+        int z = 8;
+        int a = y + z;
+        float b = 6.95f + z;
+        System.out.println(b);*/
+
+        // Increment And Deceremnet
+        int a = 78;
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a--);
+        System.out.println(--a);
+    }
+
+}
