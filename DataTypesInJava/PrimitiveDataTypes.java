a0ec9a32b3c3d86efed6b7818753c619effdb3a9
gauravkamat17 <kamatgaurav17@gmail.com>
2024-08-01 18:44:00 +0000

    Learning Java

---
+++ DataTypesInJava/PrimitiveDataTypes.java
@@ -0,0 +1,27 @@
+// Data Type specify the different types of values tha are stored in variable.
+// Primitive data type is classification of data that tells a compiler or interperter  how a programmer intended to use the data.
+// Java has 8 types of primitive data tpyes. 
+public class PrimitiveDataTypes
+{
+    public static void main(String args[])
+    {
+        boolean a = true; // booean Data Type
+        short b =  1000; // short Data Type
+        int c = 1515; // Integer Data Type
+        long d = 11121223321L; // Long Data Type
+        float e = 213.5f; // Float Data Type
+        double f =  21.3; // Double Data Type
+        byte g = 2; // Byte Data Type
+        char gaurav = 'A'; // Character Data Type
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(gaurav);
+
+
+    }
+}
