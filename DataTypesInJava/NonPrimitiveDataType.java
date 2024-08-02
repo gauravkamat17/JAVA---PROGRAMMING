958efe2a50849c119435af77d016cac6053ad3ac
gauravkamat17 <kamatgaurav17@gmail.com>
2024-08-02 12:06:56 +0000

    Learning Java

---
+++ DataTypesInJava/NonPrimitiveDataType.java
@@ -0,0 +1,10 @@
+// Non-Primitive Data Type are also known as Reference Data Type.
+// These Data Types consists (Array, String, Default)
+class NonPrimitiveDataType{
+    public static void main(String ars[]){
+        String a = "Gaurav";
+        String s1 = new String("Gaurav");
+        System.out.println(a);
+        System.out.println(s1);
+    }
+}
